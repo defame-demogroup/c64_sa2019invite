4c19d3b6044e7402216fde106ce2eb139f567d4d
Joe Cincotta <joe@pixolut.com>
2019-07-03 09:23:06 +1000

    WIP / broken

--- rsrc/bmstatemachine.asm
+++ rsrc/bmstatemachine.asm
@@ -14,64 +14,79 @@ $4338 - $471F   Color RAM
 .label v_background = v_bitmap + $2329
 .label v_colorram = v_bitmap + $2338 
 
-.label sm_count = 2 //state machine count per line
+.label sm_count = 3 //state machine count per line
+.label sm_delay = 2 //interframe delay for updates
+.label sm_done_flag = $ff
 
 .label r_screen = $4000
 .label r_colorram = $d800
 .label r_bitmap = $6000
 
+.label screen_width = 40
+.label screen_height = 25
 
 
+.function setupBitmapOffsets(){
+    .var bitmap_offsets = List()
+    .for(var i=0;i<screen_width;i++){
+        bitmap_offsets.add(i * 8)
+    }
+    .return bitmap_offsets
+}
 
 
+//dont call this in an IRQ!!!!
+.macro _initStateMachineRun(){
+    lda #$01
+    sta SM_COMPLETED
+    lda #$00
+    //fastest loop with a comparison
+    ldx #40
+!:
+    .for(var i=0;i<screen_height;i++){
+        sta SM_FINISHED + (screen_width * i) - 1,x
+    }
+    dex
+    bne !-
+}
+
+//load effect data
 
 
-    /*
-  read delay
-    count down
-    not done? return
-
-  read state
-    if 1
-        75
-    if 2
-        50
-    if 3 
-        25
-    if 4
-        0 + swap bitmap
-    if 5
-        25
-    if 6
-        50
-    if 7
-        75
-    if 8
-        100
-    if 9    
-        add offset
-        at finish?
-        no? reset delay
-        yes? set done.
-    */
+/*
+macro to insert a state machine
+*/
 .macro _transitionStateMachine(row,smIndex){
-    ldx delay: #$00
+    ldx delay: #$01
     dex
     beq !+
     stx delay
     rts
 !:
-    //reset delat here 
+    //reset delay here
+    ldx #sm_delay
+    stx delay
 
-    ldx SM_OFFSETS + (25 * sm_count) + row
-
+    //read the current position of the statemachine into X
+    ldx SM_OFFSETS + (screen_height * smIndex) + row
+    //check if this state machine is finished
+    cpx #sm_done_flag
+    bne !+
+    rts //state machine is done and waiting for others to finish
+!:
+    //this is trying to be a switch statement 
+    //by using the state, multiply by 4 and use
+    //use that as the offset into a jump table 
+    //which is why the NOPs are essential here
     lda state: #$00
     asl
     asl
     sta offset
     clc
-    bcc offset: !+
+    bcc offset: !+ //this is irrelevent as it is replaced
 !:
+    jmp init
+    nop
     jmp o75
     nop
     jmp o50
@@ -87,8 +102,12 @@ $4338 - $471F   Color RAM
     jmp n75
     nop
     jmp n100
+init:
+    //initial offset
+    lda SM_DELAYS + (screen_height * smIndex) + row
+    sta delay
+
 o75:
-    
     lda r_colorram (row * 40),x
     sta originalColorRam50
     sta originalColorRam25
@@ -136,7 +155,31 @@ swap:
     sta r_colorram + (row * 40),x
     sta r_screen + (row * 40),x
 
-//todo swap bitmap
+    //set up base addresses for bitmap copy
+    clc
+    lda SM_BITMAP_OFFSETS_LO,x
+    adc # <v_bitmap + (row * 8 * screen_width)
+    sta map_src
+    lda SM_BITMAP_OFFSETS_HI,x
+    adc # >v_bitmap + (row * 8 * screen_width)
+    sta map_src + 1
+
+    clc
+    lda SM_BITMAP_OFFSETS_LO,x
+    adc # <r_bitmap + (row * 8 * screen_width)
+    sta map_dst
+    lda SM_BITMAP_OFFSETS_HI,x
+    adc # >r_bitmap + (row * 8 * screen_width)
+    sta map_dst + 1
+
+    //copy bitmap data
+    ldy #$00
+!:
+    lda map_src: $ffff,y //source 
+    sta map_dst: $ffff,y //destination
+    iny
+    cpy #$08
+    bne !-
 
     inc state
     rts
@@ -186,8 +229,34 @@ n100:
     lda v_screen + (row * 40),x
     sta r_screen + (row * 40),x
 
-//update offset using delta here
+    //now set up machine for next move
+    lda  SM_DELTAS + (screen_height * smIndex) + row
+    and #%10000000
+    beq backwards
 
+forwards:
+    clc
+    lda SM_OFFSETS + (screen_height * smIndex) + row
+    adc SM_DELTAS + (screen_height * smIndex) + row
+    sta SM_OFFSETS + (screen_height * smIndex) + row
+    jmp finished
+
+backwards:
+    sec
+    lda SM_OFFSETS + (screen_height * smIndex) + row
+    sbc SM_DELTAS + (screen_height * smIndex) + row
+    sta SM_OFFSETS + (screen_height * smIndex) + row
+
+finished:
+    lda #$01 //never use zero as that is for init only
+    sta state
+    
+    lda #$00
+    sta SM_COMPLETED //negate a complete flag
+    rts
+
+state_machine_completed:
+    //reset state machine
     lda #$00
     sta state
     rts
@@ -201,19 +270,59 @@ DATASETS
 *********************************************/
 .align $100
 .pc = * "state machine number"
-SM_OFFSETS:
-.for(var i=0;i<(25 * sm_count);i++){
+SM_OFFSETS: //x offset of each statemachine (sets initial start location and then updated per frame)
+.for(var i=0;i<(screen_height * sm_count);i++){
     .byte $00
 }
 
-SM_DELTAS:
-.for(var i=0;i<(25 * sm_count);i++){
+SM_DELTAS: //distance deltas of each state machine - high bit is subtraction
+.for(var i=0;i<(screen_height * sm_count);i++){
     .byte $00
 }
 
-SM_BITMAP_OFFSET_HI:
-.
+SM_DELAYS: //initial frame delays for each state machine
+.for(var i=0;i<(screen_height * sm_count);i++){
+    .byte $00
+}
 
-SM_BITMAP_OFFSET_LO:
+SM_FINISHED: //mark completed states
+.for(var i=0;i<(screen_height);i++){
+    .for(var j=0;j<screen_width;j++){
+        .byte $00
+    }
+}
 
+SM_COMPLETED: //endstate when stat machine is done
+.byte $00
+//set to $01 initially and SM only set to zero if not done
+
+
+
+.var bitmap_offsets = setupBitmapOffsets()
+
+SM_BITMAP_OFFSETS_LO:
+.for(var i=0; i<screen_width;i++){
+    .byte <bitmap_offsets.get(i)
+}
+
+SM_BITMAP_OFFSETS_HI:
+.for(var i=0; i<screen_width;i++){
+    .byte >bitmap_offsets.get(i)
+}
+
+EFFECT_1:
+SM_OFFSETS: //x offset of each statemachine (sets initial start location and then updated per frame)
+.byte $00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00
+.byte $01,$01,$01,$01,$01,$01,$01,$01,$01,$01,$01,$01,$01,$01,$01,$01,$01,$01,$01,$01,$01,$01,$01,$01,$01
+.byte $02,$02,$02,$02,$02,$02,$02,$02,$02,$02,$02,$02,$02,$02,$02,$02,$02,$02,$02,$02,$02,$02,$02,$02,$02
+
+SM_DELTAS: //distance deltas of each state machine - high bit is subtraction
+.byte $03,$03,$03,$03,$03,$03,$03,$03,$03,$03,$03,$03,$03,$03,$03,$03,$03,$03,$03,$03,$03,$03,$03,$03,$03
+.byte $03,$03,$03,$03,$03,$03,$03,$03,$03,$03,$03,$03,$03,$03,$03,$03,$03,$03,$03,$03,$03,$03,$03,$03,$03
+.byte $03,$03,$03,$03,$03,$03,$03,$03,$03,$03,$03,$03,$03,$03,$03,$03,$03,$03,$03,$03,$03,$03,$03,$03,$03
+
+SM_DELAYS: //initial frame delays for each state machine
+.byte $01,$01,$01,$01,$01,$01,$01,$01,$01,$01,$01,$01,$01,$01,$01,$01,$01,$01,$01,$01,$01,$01,$01,$01,$01
+.byte $02,$02,$02,$02,$02,$02,$02,$02,$02,$02,$02,$02,$02,$02,$02,$02,$02,$02,$02,$02,$02,$02,$02,$02,$02
+.byte $03,$03,$03,$03,$03,$03,$03,$03,$03,$03,$03,$03,$03,$03,$03,$03,$03,$03,$03,$03,$03,$03,$03,$03,$03
 
